6216460262c72f13da64a17773aeb22ee7bbf9b8
vishnufr <vishnu.heddese@gmail.com>
2025-12-02 14:27:36 -0600

    Deploy static build to docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  basePath: '/sales-commission-dashboard',
+  assetPrefix: '/sales-commission-dashboard/'
 };
 
 export default nextConfig;
